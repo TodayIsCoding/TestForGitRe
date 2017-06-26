ded277be310fc25b9e3c6f820791829eca532614
today <fengjintian@sina.cn>
2017-06-26 16:06:42 +0800

    在分支version1.0上添加一个变量b

--- app/src/main/java/com/example/coolweather/MainActivity.java
+++ app/src/main/java/com/example/coolweather/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a = 10;
+        int b = 2;
     }
 
     public void showToast(View view) {
